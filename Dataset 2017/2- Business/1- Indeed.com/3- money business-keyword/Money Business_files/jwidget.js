b080b8452166cacfbe050b976fbddfbef22dad46
Fadwa Estuka <fadwa.estuka@gmail.com>
2018-03-16 02:51:59 -0600

    Add files via upload

---
+++ Dataset 2017/2- Business/1- Indeed.com/3- money business-keyword/Money Business_files/jwidget.js
@@ -0,0 +1,14 @@
+
+window['indeed_clk'] = function(a,sig) { var hr = a.href; var si = hr.indexOf('&jsa='); if (si > 0) return; var jsh = hr + '&jsa=' + sig; if ( jsh.indexOf('&inchal') == -1 ) jsh += '&inchal=jswresults'; a.href = jsh; }
+
+if (window['indeedJobroll'] != undefined) {
+    window['indeedJobroll']['jobsCallback']('indJobContent', '    <div id=\"featemp_results\">\n    <div id=\"job_0\" class=\"job\">\n        <a target=\"_blank\" href=\"https:\/\/ca.indeed.com\/pagead\/clk?mo=r&ad=-6NYlbfkN0Ax9__7PR76iCPyXqtYHGHyHp4qkGxFmPLMp-thEvSCW5uLh3TAsNsol9m6R53msCbZAnTz7BTpFNgEuK76wb1UU6vmOGA49KykYb-VXuDjoVvhTtAjqHUhcbr10Es__5PqHFskRu4aEn6NRI5Lj00gwYkx2IpYDTZ3RxalcWcNa6Yl_u_iqeqSvVK1aX68P49Sk-hW8UFciXpoBP8z3xkgUTYx7IOFgHgFxd_yA2VUmDf6bgIV7deQXi3uW_pbQjFShCTS3K_WA_9Sxx7cz2cdyxJLmpCO0kN2jS0Dmyr4F82pd3ykwJFwFfXI9hk34yRA43UajiyXOU9uj_mVCmuJj2yUp4k2FO133TUR8W6ViOplLef4ujF4Cwr-WnQsx9A1uxIhbtgMa1cS8Dov57wRUH26fT16T9ETnvQlTNiSaJg_wvp76-X4i9RBPsSWUyyT_NWedTxbHuChomL_pxfHMjoDo83PPG3DWRPFERWHMoaWr8_9LqLKzPrxscEMnXUigATrCgf0rNVIv-5y78ndl13m_jY-djx1Ok9JnWSrWEkffek-i-D9eMvu8aB7frJUaLBl-KeCDUSwgXmZF6Zy&atk=\" class=\"jobtitle\" onmousedown=\"indeed_clk(this,\'2908\');\">Travel Abroad English Teacher (China), NO Experience (NEED...<\/a>\n    <\/div>\n    <\/div>\n');
+} else {
+    var ind_jobs_el = document.getElementById('indJobContent');
+    var ind_err = null;
+    if (ind_jobs_el) {
+        ind_jobs_el.innerHTML='    <div id=\"featemp_results\">\n    <div id=\"job_0\" class=\"job\">\n        <a target=\"_blank\" href=\"https:\/\/ca.indeed.com\/pagead\/clk?mo=r&ad=-6NYlbfkN0Ax9__7PR76iCPyXqtYHGHyHp4qkGxFmPLMp-thEvSCW5uLh3TAsNsol9m6R53msCbZAnTz7BTpFNgEuK76wb1UU6vmOGA49KykYb-VXuDjoVvhTtAjqHUhcbr10Es__5PqHFskRu4aEn6NRI5Lj00gwYkx2IpYDTZ3RxalcWcNa6Yl_u_iqeqSvVK1aX68P49Sk-hW8UFciXpoBP8z3xkgUTYx7IOFgHgFxd_yA2VUmDf6bgIV7deQXi3uW_pbQjFShCTS3K_WA_9Sxx7cz2cdyxJLmpCO0kN2jS0Dmyr4F82pd3ykwJFwFfXI9hk34yRA43UajiyXOU9uj_mVCmuJj2yUp4k2FO133TUR8W6ViOplLef4ujF4Cwr-WnQsx9A1uxIhbtgMa1cS8Dov57wRUH26fT16T9ETnvQlTNiSaJg_wvp76-X4i9RBPsSWUyyT_NWedTxbHuChomL_pxfHMjoDo83PPG3DWRPFERWHMoaWr8_9LqLKzPrxscEMnXUigATrCgf0rNVIv-5y78ndl13m_jY-djx1Ok9JnWSrWEkffek-i-D9eMvu8aB7frJUaLBl-KeCDUSwgXmZF6Zy&atk=\" class=\"jobtitle\" onmousedown=\"indeed_clk(this,\'2908\');\">Travel Abroad English Teacher (China), NO Experience (NEED...<\/a>\n    <\/div>\n    <\/div>\n';
+    } else {
+        ind_err = "no_elt";
+    }
+}
