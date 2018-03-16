2667697e48557016acf5c07c0b800b92f2eee19f
Fadwa Estuka <fadwa.estuka@gmail.com>
2018-03-16 02:20:46 -0600

    Add files via upload

---
+++ Dataset 2017/1- Arts/5- Scribd.com/1- martial arts-keyword/Search _ Scribd_files/init1.js
@@ -0,0 +1,16 @@
+//<script>
+bouncex.state = {"newvid":false,"iol":false,"tn":1485306348,"pvid":11,"rc":false,"mobile":false,"vip":"142.244.63.135"};
+bouncex.cookie = {"v":{"ever_logged_in":false,"active_archive_purchase":false,"pmp_subscriber":false,"doc_title":false,"doc_short_title":false,"doc_category":false,"doc_type":false,"ibx_test":false,"interest":false,"campaign_timer":false,"sub_reported":false},"sid":11,"fvt":1485306348,"vid":1485305749951310,"ao":0,"lp":"https%3A%2F%2Fwww.scribd.com%2F","as":0,"vpv":11,"d":"d","r":"","cvt":1485305749,"gcr":20,"m":0,"did":"8289404833774682656","lvt":1485306348};
+bouncex.brandStyles = false;
+bouncex.webfonts = false;
+bouncex.campaigns = false;
+bouncex.gbi.stacks = false;
+bouncex.debug = false;
+
+if(bouncex.browser.msie){
+	setTimeout(function(){
+		bouncex.init();
+	}, 0);
+} else {
+	bouncex.init();
+}
