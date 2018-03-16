dfd7e9c8685c5cbfc6b281f967ddf8c0063a8151
Fadwa Estuka <fadwa.estuka@gmail.com>
2018-03-16 02:34:49 -0600

    Add files via upload

---
+++ Dataset 2017/1- Arts/7- Ign.com/2- ancient arts-keyword/Search Results_files/iframe_data/core_id.js
@@ -0,0 +1,1 @@
+var _zd_core_id;if(!localStorage||!(_zd_core_id=localStorage.getItem('_zd_core_id'))){var _zd_core_id='9b97656e91094b42b224f9daf39252cd';localStorage.setItem('_zd_core_id',_zd_core_id)}var value='; '+document.cookie,parts=value.split('; h_zdbb='),zdbb='';2==parts.length&&(zdbb=parts.pop().split(';').shift()),parent.postMessage({core_id:_zd_core_id,zdbb:zdbb},'*');
