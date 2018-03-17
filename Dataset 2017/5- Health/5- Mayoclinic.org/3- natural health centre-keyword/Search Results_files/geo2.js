b6b58cf82121c997ed46b1042f7023dbdc376409
Fadwa Estuka <fadwa.estuka@gmail.com>
2018-03-17 02:57:45 -0600

    Add files via upload

---
+++ Dataset 2017/5- Health/5- Mayoclinic.org/3- natural health centre-keyword/Search Results_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "EDMONTON",
+      'continent': "NA",
+      'country': "CA",
+      'region': "AB"
+    },
+    'ip':"142.244.63.135"
+  }]);
+})
+//
+()
+
+;
