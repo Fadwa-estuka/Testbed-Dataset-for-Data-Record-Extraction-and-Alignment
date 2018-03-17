b9e53d670babd96d4511f98600dc7b1d06884f25
Fadwa Estuka <fadwa.estuka@gmail.com>
2018-03-17 03:02:39 -0600

    Add files via upload

---
+++ Dataset 2017/5- Health/9- Psychologytoday.com/2- digestive-keyword/Search _ Psychology_files/js_YEJeobnrIUhwCCVkwwqJeVvRYAhyi2g-mYe2WzlfUDw.js
@@ -0,0 +1,204 @@
+(function ($) {
+
+/**
+ * Toggle the visibility of a fieldset using smooth animations.
+ */
+Drupal.toggleFieldset = function (fieldset) {
+  var $fieldset = $(fieldset);
+  if ($fieldset.is('.collapsed')) {
+    var $content = $('> .fieldset-wrapper', fieldset).hide();
+    $fieldset
+      .removeClass('collapsed')
+      .trigger({ type: 'collapsed', value: false })
+      .find('> legend span.fieldset-legend-prefix').html(Drupal.t('Hide'));
+    $content.slideDown({
+      duration: 'fast',
+      easing: 'linear',
+      complete: function () {
+        Drupal.collapseScrollIntoView(fieldset);
+        fieldset.animating = false;
+      },
+      step: function () {
+        // Scroll the fieldset into view.
+        Drupal.collapseScrollIntoView(fieldset);
+      }
+    });
+  }
+  else {
+    $fieldset.trigger({ type: 'collapsed', value: true });
+    $('> .fieldset-wrapper', fieldset).slideUp('fast', function () {
+      $fieldset
+        .addClass('collapsed')
+        .find('> legend span.fieldset-legend-prefix').html(Drupal.t('Show'));
+      fieldset.animating = false;
+    });
+  }
+};
+
+/**
+ * Scroll a given fieldset into view as much as possible.
+ */
+Drupal.collapseScrollIntoView = function (node) {
+  var h = document.documentElement.clientHeight || document.body.clientHeight || 0;
+  var offset = document.documentElement.scrollTop || document.body.scrollTop || 0;
+  var posY = $(node).offset().top;
+  var fudge = 55;
+  if (posY + node.offsetHeight + fudge > h + offset) {
+    if (node.offsetHeight > h) {
+      window.scrollTo(0, posY);
+    }
+    else {
+      window.scrollTo(0, posY + node.offsetHeight - h + fudge);
+    }
+  }
+};
+
+Drupal.behaviors.collapse = {
+  attach: function (context, settings) {
+    $('fieldset.collapsible', context).once('collapse', function () {
+      var $fieldset = $(this);
+      // Expand fieldset if there are errors inside, or if it contains an
+      // element that is targeted by the URI fragment identifier.
+      var anchor = location.hash && location.hash != '#' ? ', ' + location.hash : '';
+      if ($fieldset.find('.error' + anchor).length) {
+        $fieldset.removeClass('collapsed');
+      }
+
+      var summary = $('<span class="summary"></span>');
+      $fieldset.
+        bind('summaryUpdated', function () {
+          var text = $.trim($fieldset.drupalGetSummary());
+          summary.html(text ? ' (' + text + ')' : '');
+        })
+        .trigger('summaryUpdated');
+
+      // Turn the legend into a clickable link, but retain span.fieldset-legend
+      // for CSS positioning.
+      var $legend = $('> legend .fieldset-legend', this);
+
+      $('<span class="fieldset-legend-prefix element-invisible"></span>')
+        .append($fieldset.hasClass('collapsed') ? Drupal.t('Show') : Drupal.t('Hide'))
+        .prependTo($legend)
+        .after(' ');
+
+      // .wrapInner() does not retain bound events.
+      var $link = $('<a class="fieldset-title" href="#"></a>')
+        .prepend($legend.contents())
+        .appendTo($legend)
+        .click(function () {
+          var fieldset = $fieldset.get(0);
+          // Don't animate multiple times.
+          if (!fieldset.animating) {
+            fieldset.animating = true;
+            Drupal.toggleFieldset(fieldset);
+          }
+          return false;
+        });
+
+      $legend.append(summary);
+    });
+  }
+};
+
+})(jQuery);
+;
+/**
+ * @file
+ * A JavaScript file for the theme.
+ * This file should be used as a template for your other js files.
+ * It defines a drupal behavior the "Drupal way".
+ *
+ */
+
+// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - https://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+
+(function ($, Drupal, window, document, undefined) {
+  'use strict';
+
+  // To understand behaviors, see https://drupal.org/node/756722#behaviors
+  Drupal.behaviors.hideSubmitBlockit = {
+    attach: function(context) {
+      var timeoutId = null;
+      $('form', context).once('hideSubmitButton', function () {
+        var $form = $(this);
+
+        // Bind to input elements.
+        $('input.form-submit, button.form-submit', $form).click(function (e) {
+          var el = $(this);
+          el.after('<input type="hidden" name="' + el.attr('name') + '" value="' + el.attr('value') + '" />');
+          return true;
+        });
+
+        // Bind to form submit.
+        $('form', context).submit(function (e) {
+          var $inp;
+          if (!e.isPropagationStopped()) {
+            if (Drupal.settings.hide_submit.hide_submit_method === 'disable') {
+              $('input.form-submit, button.form-submit', $form).attr('disabled', 'disabled').each(function (i) {
+                var $button = $(this);
+                if (Drupal.settings.hide_submit.hide_submit_css) {
+                  $button.addClass(Drupal.settings.hide_submit.hide_submit_css);
+                }
+                if (Drupal.settings.hide_submit.hide_submit_abtext) {
+                  $button.val($button.val() + ' ' + Drupal.settings.hide_submit.hide_submit_abtext);
+                }
+                $inp = $button;
+              });
+
+              if ($inp && Drupal.settings.hide_submit.hide_submit_atext) {
+                $inp.after('<span class="hide-submit-text">' + Drupal.checkPlain(Drupal.settings.hide_submit.hide_submit_atext) + '</span>');
+              }
+            }
+            else {
+              var pdiv = '<div class="hide-submit-text' + (Drupal.settings.hide_submit.hide_submit_hide_css ? ' ' + Drupal.checkPlain(Drupal.settings.hide_submit.hide_submit_hide_css) + '"' : '') + '>' + Drupal.checkPlain(Drupal.settings.hide_submit.hide_submit_hide_text) + '</div>';
+              if (Drupal.settings.hide_submit.hide_submit_hide_fx) {
+                $('input.form-submit, button.form-submit', $form).addClass(Drupal.settings.hide_submit.hide_submit_css).fadeOut(100).eq(0).after(pdiv);
+                $('input.form-submit, button.form-submit', $form).next().fadeIn(100);
+              }
+              else {
+                $('input.form-submit, button.form-submit', $form).addClass(Drupal.settings.hide_submit.hide_submit_css).hide().eq(0).after(pdiv);
+              }
+            }
+            // Add a timeout to rerset the buttons (if needed).
+            if (Drupal.settings.hide_submit.hide_submit_reset_time) {
+              timeoutId = window.setTimeout(function() {
+                hideSubmitResetButtons(null, $form);
+              }, Drupal.settings.hide_submit.hide_submit_reset_time);
+            }
+          }
+          return true;
+        });
+      });
+
+      // Bind to clientsideValidationFormHasErrors to support clientside validation.
+      // $(document).bind('clientsideValidationFormHasErrors', function(event, form) {
+        //hideSubmitResetButtons(event, form.form);
+      // });
+
+      // Reset all buttons.
+      function hideSubmitResetButtons(event, form) {
+        // Clear timer.
+        window.clearTimeout(timeoutId);
+        timeoutId = null;
+        if (Drupal.settings.hide_submit.hide_submit_method === 'disable') {
+          $('input.' + Drupal.checkPlain(Drupal.settings.hide_submit.hide_submit_css) + ', button.' + Drupal.checkPlain(Drupal.settings.hide_submit.hide_submit_css), form)
+            .removeClass(Drupal.checkPlain(Drupal.settings.hide_submit.hide_submit_hide_css))
+            .removeAttr('disabled');
+          $('.hide-submit-text', form).remove();
+        }
+        else {
+          $('input.' + Drupal.checkPlain(Drupal.settings.hide_submit.hide_submit_css) + ', button.' + Drupal.checkPlain(Drupal.settings.hide_submit.hide_submit_css), form)
+            .stop()
+            .removeClass(Drupal.checkPlain(Drupal.settings.hide_submit.hide_submit_hide_css))
+            .show();
+          $('.hide-submit-text', form).remove();
+        }
+      }
+    }
+  };
+
+})(jQuery, Drupal, window, this.document);
+;
