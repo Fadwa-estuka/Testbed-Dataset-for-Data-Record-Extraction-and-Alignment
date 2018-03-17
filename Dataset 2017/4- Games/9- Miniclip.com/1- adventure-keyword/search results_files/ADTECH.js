d497194e9f498f28c817736e1d6bd594ed7a4591
Fadwa Estuka <fadwa.estuka@gmail.com>
2018-03-17 02:32:19 -0600

    Add files via upload

---
+++ Dataset 2017/4- Games/9- Miniclip.com/1- adventure-keyword/search results_files/ADTECH.js
@@ -0,0 +1,14 @@
+var matchingSizeLink4420367 = [154,170,529,2988,3055,225];	
+var matchingSize4420367 = [ 16,277,509,1013,2898,4057,4296,4796,6251,6567,6568,6744 ];
+var found_4420367 = matchingSize4420367.indexOf(16); 
+var foundLinkSize_4420367 = matchingSizeLink4420367.indexOf(16);
+if (found_4420367 > -1 ) {
+	document.write('<img src="http://aka-cdn-ns.adtechus.com/images/Default_Size_16_1x1.gif" border=0 alt="" width="1" height="1" >');
+}
+else if (foundLinkSize_4420367 > -1) {
+	document.write('<a href="http://www.playwire.com/" target=_blank><img src="http://aka-cdn-ns.adtechus.com/images/Default_Size_16_1x1.gif" border=0 alt="AdTech Ad" ></a>');
+}
+else {
+	document.write('<a href="http://www.gamezone.com/news/destiny-xur-agent-of-the-nine-location-and-exotic-items-10-24-14" target=_blank><img src="http://aka-cdn-ns.adtechus.com/images/Default_Size_16_1x1.gif" border=0 alt="AdTech Ad" ></a>');
+}
+  
