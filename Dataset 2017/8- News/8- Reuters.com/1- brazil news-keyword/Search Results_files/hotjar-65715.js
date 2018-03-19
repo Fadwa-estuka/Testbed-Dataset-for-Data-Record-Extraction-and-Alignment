76993faab93aa568655845cab49ad0662cd9c02e
Fadwa Estuka <fadwa.estuka@gmail.com>
2018-03-18 20:11:24 -0600

    Add files via upload

---
+++ Dataset 2017/8- News/8- Reuters.com/1- brazil news-keyword/Search Results_files/hotjar-65715.js
@@ -0,0 +1,54 @@
+window.hjSiteSettings = window.hjSiteSettings || {"testers_widgets":[],"surveys":[],"record_targeting_rules":[],"recording_capture_keystrokes":true,"polls":[],"site_id":65715,"forms":[],"record":false,"heatmaps":[{"created_epoch_time":1477419522,"targeting":[{"negate":false,"pattern":"http:\/\/www.reuters.com\/finance\/funds","match_operation":"exact","component":"url"}],"id":862316,"selector_version":0},{"created_epoch_time":1477419341,"targeting":[{"negate":false,"pattern":"http:\/\/www.reuters.com\/finance\/summits","match_operation":"simple","component":"url"}],"id":862302,"selector_version":0},{"created_epoch_time":1477419298,"targeting":[{"negate":false,"pattern":"http:\/\/www.reuters.com\/subjects\/us-lipper-awards","match_operation":"simple","component":"url"}],"id":862298,"selector_version":0}],"deferred_page_contents":[],"feedback_widgets":[],"r":0.1450412004,"state_change_listen_mode":"manual"};
+
+window.hjBootstrap = window.hjBootstrap || function (scriptUrl) {
+    var b = function () {}, d = document, h = d.head || d.getElementsByTagName('head')[0], s, v, c, ct;
+
+    if (!d.addEventListener) {
+        return;
+    }
+
+    s = d.createElement('script');
+    s.src = scriptUrl;
+    h.appendChild(s);
+
+    ct = [
+        'iframe#_hjRemoteVarsFrame {',
+        'display: none !important; width: 1px !important; height: 1px !important; ' +
+        'opacity: 0 !important; pointer-events: none !important;',
+        '}'
+    ];
+    c = document.createElement('style');
+    c.type = 'text/css';
+    if (c.styleSheet) {
+        c.styleSheet.cssText = ct.join('');
+    } else {
+        c.appendChild(d.createTextNode(ct.join('')));
+    }
+    h.appendChild(c);
+
+    v = d.createElement('iframe');
+    v.style.cssText = ct[1];
+    v.name = '_hjRemoteVarsFrame';
+    v.title = 'Hotjar Remote Vars Frame';
+    v.id = '_hjRemoteVarsFrame';
+    v.src = 'https://' + (window._hjSettings.varsHost || 'vars.hotjar.com') + '/rcj-b2c1bce0a548059f409c021a46ea2224.html';
+    v.onload = function () {
+        b.varsLoaded = true;
+        if ((typeof hj != 'undefined') && hj.event) {
+            hj.event.signal('varsLoaded');
+        }
+    };
+    b.varsJar = v;
+
+    if (d.body) {
+        d.body.appendChild(v);
+    } else {
+        d.addEventListener('DOMContentLoaded', function () {
+            d.body.appendChild(v);
+        });
+    }
+    window.hjBootstrap = b;
+};
+
+
+hjBootstrap('https://script.hotjar.com/modules-c32705eea471a3b86fa01dc951b20d98.js');
