b9f21d003e4962855034d0fcb08df34149c807b4
Fadwa Estuka <fadwa.estuka@gmail.com>
2018-04-22 23:37:23 -0600

    Add files via upload

---
+++ Dataset 2017/13- Shopping/8- Newegg.com/2- mobile phones-keyword/Search Results_files/637300.js
@@ -0,0 +1,4 @@
+(function(c,h,a,t,_,i,d){c[_]=c[_]||function(){
+    (c[_].q=c[_].q||[]).push(arguments)},c[_].l=1*new Date();i=h.createElement(a),
+    d=h.getElementsByTagName(a)[0];i.async=1;i.src=t;d.parentNode.insertBefore(i,d)
+})(window,document,'script','https://chatidcdn.com/chatbar/newegg.com/latest.js','CID');
