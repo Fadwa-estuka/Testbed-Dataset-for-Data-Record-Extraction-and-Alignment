09b8555bec763c762b295ae979f18478e7f2ddf2
Fadwa Estuka <fadwa.estuka@gmail.com>
2018-04-23 00:56:43 -0600

    Add files via upload

---
+++ Dataset 2017/13- Shopping/9- Hm.com/1- jeans-keyword/Search Results_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "EDMONTON",
+      'continent': "NA",
+      'country': "CA",
+      'region': "AB"
+    },
+    'ip':"75.152.114.235"
+  }]);
+})
+//
+()
+
+;
